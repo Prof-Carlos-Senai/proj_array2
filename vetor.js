b7c083ab99f6ae344fc9fc371bdd63035c48a52d
Carlos <silva@edu.sc.senai.br>
2023-03-15 10:37:56 -0300

    função inserir no início e excluir no início

--- vetor.js
+++ vetor.js
@@ -9,10 +9,23 @@ function inserir(){
 
 }
 
+function insert_inicio(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
 function excluir(){
     valor_cidade.pop()
     console.log(valor_cidade)
     res.innerHTML = valor_cidade
+}
+
+function excluir_incio(){
+    valor_cidade.shift()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade 
 }
 
 
