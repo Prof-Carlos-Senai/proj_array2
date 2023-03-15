62532a2d153d37a4be397dacca43917a10d23999
Carlos <silva@edu.sc.senai.br>
2023-03-15 09:39:41 -0300

    incluindo innerHTML para imprimir na tela

--- vetor.js
+++ vetor.js
@@ -1,12 +1,18 @@
 let valor_cidade = []   // variavel global do tipo array
+let res = document.getElementById('res')
 
 function inserir(){
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+
 }
 
 function excluir(){
     valor_cidade.pop()
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
+
+
